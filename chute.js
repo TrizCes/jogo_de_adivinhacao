90d51eeb2f2b94853bd327af363473669612ead4
Beatriz Ceschini <biaceschini@gmail.com>
2024-02-07 16:30:53 -0300

    adequações de css e funcionalidades

--- chute.js
+++ chute.js
@@ -4,13 +4,11 @@ const iniciarNovoJogo = document.getElementById('newGame');
 const resposta = document.getElementById('response');
 const mensagemDica = document.getElementById('message');
 
-let numero = Math.floor(Math.random() * 10 + 1);
+let numero = Math.floor(Math.random() * 100 + 1);
 const tentativas = [];
 
 const jogo = () => {
-  console.log(chute.value);
   const palpite = parseInt(chute.value);
-  console.log('jogando');
   tentativas.push(palpite);
   if (palpite === numero) {
     const gratificacao = resposta.appendChild(document.createElement('h2'));
@@ -20,7 +18,10 @@ const jogo = () => {
     mensagemParagrafo.innerText = `Você acertou o número na ${tentativas.length}ª tentativa`;
 
     iniciarPalpite.disabled = true;
+    iniciarPalpite.style.display = 'none';
     iniciarNovoJogo.disabled = false;
+    iniciarNovoJogo.style.display = 'flex';
+    chute.style.backgroundColor = 'rgb(5, 119, 190)';
   } else {
     mensagemDica.innerText = 'Continue jogando, a sorte encontra que persiste!';
   }
@@ -29,8 +30,15 @@ const jogo = () => {
 const novoJogo = () => {
   numero = Math.floor(Math.random() * 10 + 1);
   mensagemDica.innerText = '';
+
   iniciarPalpite.disabled = false;
+  iniciarPalpite.style.display = 'flex';
   iniciarNovoJogo.disabled = true;
+  iniciarNovoJogo.style.display = 'none';
+  chute.style.backgroundColor = 'rgb(5, 119, 190)';
+
   resposta.removeChild(resposta.querySelector('h2'));
   resposta.removeChild(resposta.querySelector('p'));
+  mensagemDica.innerText = '';
+  tentativas.splice(0);
 };
